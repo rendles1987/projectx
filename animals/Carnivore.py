06a56700a0b12635f220013bb481901741b0cc59
rendles1987 <renierkramer1987@gmail.com>
2019-03-08 16:44:05 +0100

    gitignore stuff

--- animals/Carnivore.py
+++
@@ -1,9 +0,0 @@
-from ..tools.controller.seq_manager import ProcessController
-from .Mammal import Mammals
-
-a = ProcessController()
-
-class Carnivore(Mammals):
-    def food(self):
-        print("food")
-        pass
